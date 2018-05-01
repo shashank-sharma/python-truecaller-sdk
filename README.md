57816cd76819ebeda435c6a395b00820052ea884
Shashank Sharma <shashank.sharma98@gmail.com>
2018-05-01 10:30:13 +0530

    Initial project added

---
+++ README.md
@@ -0,0 +1,1 @@
+# Using Truecaller SDK with Django and Heroku
---
+++ login/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Truecaller(models.Model):
+    name = models.CharField(max_length=80)
+    phone_number = models.CharField(max_length=15)
+    access_token = models.CharField(max_length=80)
+    requestId = models.CharField(max_length=80)
